7a574d5b5ff37243dc33282e6a6f74795fb2ac97
An Doan <45700333+anvdoan@users.noreply.github.com>
2020-11-23 15:30:03 -0500

    added console logs for mute

--- js/video.js
+++ js/video.js
@@ -3,18 +3,18 @@ var video;
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 	console.log('Starting');
-	console.log(video.volume)
+	console.log(video.volume);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	video.play()
+	video.play();
 	let vol= document.querySelector("#volume").innerHTML = video.volume + '%'
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video")
-	video.pause()
+	console.log("Pause Video");
+	video.pause();
 
 });
 
@@ -27,11 +27,13 @@ document.querySelector('#volumeSlider').addEventListener('change', function() {
 document.querySelector('#mute').addEventListener('click', function()){
 	if (video.muted == false){
 		video.muted = true;
-		let button= document.querySelector('#mute').innerHTML = 'Unmute'
+		let button= document.querySelector('#mute').innerHTML = 'Unmute';
+		console.log('Video Muted');
 
 	}
 	else{
-		video.muted = false
-		button = 'Mute'
+		video.muted = false;
+		button = 'Mute';
+		console.log('Video Unmuted');
 	}
 }
