4c8e22b518bb3a82e82528a8e9fb0712ba286ec5
An Doan <45700333+anvdoan@users.noreply.github.com>
2020-11-23 14:43:28 -0500

    fixed error

--- js/video.js
+++ js/video.js
@@ -8,11 +8,12 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	video.play
+	video.play()
+	let vol= document.querySelector("#volume").innerHTML = video.volume + '%'
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video")
-	video.pause
+	video.pause()
 
 });
